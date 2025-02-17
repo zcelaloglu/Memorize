88ba22668bfc4ecf512b54f450f9ec0803bd0381
zcelaloglu <zafer.celaloglu@gmail.com>
2025-02-17 23:39:12 +0300

    complete assignment 2

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -8,38 +8,42 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-    @ObservedObject var viewModel: EmojiMemoryGame = .init()
-        
+    @StateObject var viewModel = EmojiMemoryGame()
+    
     var body: some View {
         VStack {
-            title
-            Spacer(minLength: 30)
-            ScrollView {
-                cards
-            }
+            Text(viewModel.currentTheme.name)
+                .font(.title)
+                .padding()
             
-            Button("Shuffle") {
-                viewModel.shuffle()
+            Text("Score: \(viewModel.score)")
+                .font(.title2)
+                .foregroundColor(.gray)
+            
+            ScrollView {
+                cards.animation(.default, value: viewModel.cards)
             }
+            Button("New Game") {
+                viewModel.newGame()
+            }.padding().clipShape(Capsule())
             Spacer()
         }
         .padding()
     }
     
-    var title: some View {
-        Text("Memorize!")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-    }
-    
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
+            ForEach(viewModel.cards) { card in
+                VStack(spacing: 0) {
+                    CardView(card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .padding(4)
+                }.onTapGesture {
+                    viewModel.choose(card)
+                }
             }
-        }.foregroundColor(.orange)
+        }
+        .foregroundColor(viewModel.currentTheme.color)
     }
 }
 
@@ -54,8 +58,8 @@ struct ThemeButton: View {
                 Image(systemName: symbol)
                 Text(text).font(.caption2)
             }
-    })
-}}
+        })
+    }}
 
 struct CardView: View {
     let card: MemoryGame<String>.Card
@@ -75,9 +79,10 @@ struct CardView: View {
                     .minimumScaleFactor(0.01)
                     .aspectRatio(1, contentMode: .fit)
             }
-                .opacity(card.isFaceUp ? 1 : 0)
+            .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1: 0)
     }
 }
 
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,31 +7,78 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
+    private(set) var score = 0
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = []
         for pairIndex in 0..<numberOfPairsOfCards {
             let content: CardContent = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
-
+            cards.append(Card(content: content, id: "\(pairIndex + 1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)b"))
         }
+        cards.shuffle()
     }
     
-    func choose(_ card: Card) {
-        
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = newValue == $0 } }
+    }
+    
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                    score += 2
+                } else {
+                    if cards[chosenIndex].hasBeenSeen || cards[potentialMatchIndex].hasBeenSeen {
+                        score -= 1
+                    }
+                }
+                cards[chosenIndex].isFaceUp = true
+            } else {
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            cards[chosenIndex].hasBeenSeen = true
+        }
     }
     
     mutating func shuffle() {
         cards.shuffle()
-        print(cards)
     }
     
-    struct Card {
-        var isFaceUp: Bool = true
+    mutating func resetGame() {
+        for index in cards.indices {
+            cards[index].isFaceUp = false
+            cards[index].isMatched = false
+            cards[index].hasBeenSeen = false
+        }
+        score = 0
+        cards.shuffle()
+    }
+    
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        var debugDescription: String {
+            return "\(id): \(content) \(isFaceUp ? "up": "down") \(isMatched ? " matched" : "")"
+        }
+        
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
+        var hasBeenSeen = false
         let content: CardContent
+        
+        var id: String
+    }
+}
+
+extension Array {
+    var only: Element? {
+        return count == 1 ? first : nil
     }
 }
