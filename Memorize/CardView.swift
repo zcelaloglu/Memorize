84c2712ab975b1cd36bf1385cd2b328726343f78
zcelaloglu <zafer.celaloglu@gmail.com>
2025-02-24 10:30:14 +0300

    fix code review

---
+++ Memorize/CardView.swift
@@ -0,0 +1,28 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Zafer CELALOGLU on 24.02.2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -57,46 +57,12 @@ struct ContentView: View {
             ForEach(0..<emojis.count, id: \.self) { index in
                 CardView(content: emojis[index]).aspectRatio(2/3, contentMode: .fit)
             }
-        }.foregroundColor(themeColor)
+        }.foregroundStyle(themeColor)
     }
     
     private func setupTheme(emojisSet: [String], color: Color) {
         emojis = (emojisSet + emojisSet).shuffled()
         themeColor = color
-    }
-}
-
-struct ThemeButton: View {
-    let text: String
-    let symbol: String
-    let action: () -> Void
-    
-    var body: some View {
-        Button(action: action, label: {
-            VStack {
-                Image(systemName: symbol)
-                Text(text).font(.caption2)
-            }
-    })
-}}
-
-struct CardView: View {
-    let content: String
-    @State var isFaceUp: Bool = false
-    
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
-            }.opacity(isFaceUp ? 1 : 0)
-            base.fill().opacity(isFaceUp ? 0 : 1)
-        }
-        .onTapGesture {
-            isFaceUp.toggle()
-        }
     }
 }
 
